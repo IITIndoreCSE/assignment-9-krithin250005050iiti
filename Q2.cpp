92e3eb18f686bd9a7690ee9922c544eab4eb5f85
krithin250005050iiti <mems250005050@iiti.ac.in>
2025-10-17 16:49:38 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-// Define the structure 'Color'
+
 struct Color {
     int red;
     int green;
@@ -10,7 +10,7 @@ struct Color {
 };
 
 int main() {
-    // Declare and initialize an array of 5 'Color' structures
+    
     Color colors[5] = {
         {120, 60, 200},
         {45, 220, 130},
@@ -19,14 +19,13 @@ int main() {
         {200, 180, 90}
     };
 
-    // Invert the color values of the first 3 elements
+    
     for (int i = 0; i < 3; i++) {
         colors[i].red = 255 - colors[i].red;
         colors[i].green = 255 - colors[i].green;
         colors[i].blue = 255 - colors[i].blue;
     }
 
-    // Display the new RGB values for all 5 elements
     cout << "New RGB values after inversion:\n";
     for (int i = 0; i < 5; i++) {
         cout << "Color " << i + 1 << ": "
