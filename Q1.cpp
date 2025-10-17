138061d6eb43a818c6324336cf546f08c450abb3
krithin250005050iiti <mems250005050@iiti.ac.in>
2025-10-17 16:10:21 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct Item {
+
+int id;
+float cost;
+};
+
+int main (){
+  Item items[6] = {
+{1 , 30},
+{2 , 35},
+{3 , 60},
+{4 , 70.25},
+{5 , 100},
+{6,44.66},
+};
+
+for(int i = 0; i<6; i++){
+if (items[i].cost>50.00){
+cout <<"ITEM id : " << items[i].id << " cost is " << items[i].cost << endl;
+}
+}
+return 0; 
+}
+    
+    
+    
+
 
